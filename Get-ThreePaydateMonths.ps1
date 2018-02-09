d2a584266b83d511cffa184bc22a0a628b987e80
nathanael.ries <nathanael.ries@gmail.com>
2018-02-09 09:17:20 -0500

    Add files via upload

---
+++ Get-ThreePaydateMonths.ps1
@@ -0,0 +1,10 @@
+﻿$LastPayDate = (Get-Date 2018-2-9)
+$Days = 14
+$Total = $LastPayDate
+$Months = @()
+while($Total.Year -eq (Get-Date).Year){
+$Months += ($Total.Month).ToString()
+$Total = $Total.AddDays($Days)
+}
+
+($Months | Group) | Where-Object {$_.Group.Count -gt 2} | ForEach-Object {(Get-Culture).DateTimeFormat.GetMonthName($_.Name)}
